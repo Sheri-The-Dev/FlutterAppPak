1f0ec3f2f0bdfac68044e03302021e455e9e344b
Sheri-The-Dev <shayanraza804@gmail.com>
2025-03-03 13:01:12 +0500

    Updated application logic

--- lib/about_screen.dart
+++ lib/about_screen.dart
@@ -146,3 +146,4 @@ if __name__ == "__main__":
         import traceback
         traceback.print_exc()Initial project setupAdded core project structure
 Implemented base functionality
+Updated application logic
